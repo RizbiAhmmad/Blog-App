6b82b7c4b1cc05e7a3041b1bab4315ff51bcbe2c
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-01-03 17:22:06 +0600

    Fetching all posts and search or filtering by their title,name,status etc

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
   try {
-    const user =req.user;
-    if (!user){
-    return res.status(400).json({
-      error: "Unauthorized",
-    });
+    const user = req.user;
+    if (!user) {
+      return res.status(400).json({
+        error: "Unauthorized",
+      });
     }
     console.log(req.user);
     const result = await postService.createPost(req.body, user.id as string);
@@ -20,6 +21,42 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
+const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+        ? true
+        : req.query.isFeatured === "false"
+        ? false
+        : undefined
+      : undefined;
+    // console.log({isFeatured});
+
+    const status = req.query.status as PostStatus | undefined
+
+    const authorId = req.query.authorId as string | undefined;
+
+    const result = await postService.getAllPost({
+      search: searchString,
+      tags,
+      isFeatured,
+      status,
+      authorId
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: "Post creation failed",
+      details: error,
+    });
+  }
+};
+
 export const PostController = {
   createPost,
+  getAllPost,
 };
